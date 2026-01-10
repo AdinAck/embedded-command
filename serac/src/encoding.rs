9933c89f587926504073305256a29bc2e713783f
AdinAck <adinackerman@gmail.com>
2026-01-10 11:10:16 -0800

    rename cookie-cutter to serac and generalize serialize buf (#1)
    Now arrays, tuples, etc. can implement `SerializeBuf`.
    
    (This *does* break CI, but it's breaking other crates that I think may
    deprecate anyways)

---
+++ serac/src/encoding.rs
@@ -0,0 +1,14 @@
+pub mod vanilla;
+pub use vanilla::Vanilla;
+
+/// Types implement this trait to be used as indication of a specific encoding
+/// scheme.
+pub trait Encoding {
+    /// The fundamental word of the encoding scheme.
+    ///
+    /// i.e. `u8` for `[u8; ...]` mediums.
+    type Word;
+
+    /// The serialized form converted to and from by this encoding scheme.
+    type Serialized<const SIZE: usize>;
+}
